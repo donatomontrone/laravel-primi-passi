54619e5a019c234b8f251e9586cccffccbbc279d
donatomontrone <donato.montrone@outlook.com>
2023-02-07 15:13:26 +0100

    added route and new page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body class="antialiased" style="background-color: #E9F2FF;">
-    <header>
+        <header>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/">
+                    <a class="navbar-brand" href="#">
                         <img src="https://cdn.iconscout.com/icon/free/png-256/laravel-226015.png" alt="Bootstrap" width="50" height="50">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,14 +27,9 @@
                 </div>
             </nav>
         </header>
-        <main class="mt-4 text-center">
-            <h3 class="mb-5">Ti diamo il benvenuto su Laravel</h3>
-            <div class="d-flex justify-content-center">
-                <div class="spinner-border text-danger" style="width: 3rem; height: 3rem;" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
-            <p class="fs-6">Attendi il caricamento dei contenuti...</p>
+        <main class="pt-4 text-center">
+            <h1 class="mb-5 text-danger">Laravel 9</h1>
+            <img src="https://res.cloudinary.com/cloudinary-marketing/images/w_1540,h_847/f_auto,q_auto/v1649718644/Web_Assets/blog/Laravel-SDK_221328fd8c/Laravel-SDK_221328fd8c-png?_i=AA" alt="laravel" class="w-100">
         </main>
         <!-- Bootstrap script -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
